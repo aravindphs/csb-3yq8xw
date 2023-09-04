5f72aa765922b1dca7236d0103f7e772397c17ba
Code Sandbox <hello@codesandbox.io>
2023-09-04 05:49:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a54ee80cc1527b7e42f2a8472380ae88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03ced646457f0bda5ef4741b3b9ada6",
+    "url": "https://aravindphs.github.io/csb-3yq8xw/index.html"
+  },
+  {
+    "revision": "ad36e0aba4588e846032",
+    "url": "https://aravindphs.github.io/csb-3yq8xw/static/js/2.05d131db.chunk.js"
+  },
+  {
+    "revision": "6b6a7cac8ccb036cb2f9",
+    "url": "https://aravindphs.github.io/csb-3yq8xw/static/js/main.86715893.chunk.js"
+  },
+  {
+    "revision": "4cda487daedb19c9f8c4",
+    "url": "https://aravindphs.github.io/csb-3yq8xw/static/js/runtime-main.136e5fd0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://aravindphs.github.io/csb-3yq8xw/precache-manifest.a54ee80cc1527b7e42f2a8472380ae88.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://aravindphs.github.io/csb-3yq8xw/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
